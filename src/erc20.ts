e06e66d2ac79beba39ae3694dfa6e90129b9d032
samclassix <samclassix@proton.me>
2026-01-22 00:08:52 +0700

    feat: typo in import

--- src/erc20.ts
+++ src/erc20.ts
@@ -1,6 +1,6 @@
 import { ponder } from 'ponder:registry';
-import { indexERC20Balance } from './lib/ERC20Balance';
-import { indexERC20MintBurn } from './lib/ERC20MintBurn';
+import { indexERC20Balance } from './lib/erc20Balance';
+import { indexERC20MintBurn } from './lib/erc20MintBurn';
 
 ponder.on('ERC20:Transfer', async ({ event, context }) => {
 	await indexERC20MintBurn(event, context);
