b0ac80f349c400a3c71b4515619673e8e307ee41
samclassix <samclassix@proton.me>
2026-01-21 23:09:13 +0700

    feat: overwrite module messenges

--- src/stablecoin.ts
+++ src/stablecoin.ts
@@ -4,7 +4,7 @@ import {
 	StablecoinModuleHistory,
 	StablecoinModuleMapping,
 } from '../schema/stablecoin';
-import { maxUint256 } from 'viem';
+import { Address, maxUint256 } from 'viem';
 
 ponder.on('Stablecoin:SubmitModule', async ({ event, context }) => {
 	await context.db.insert(StablecoinModuleHistory).values({
@@ -64,8 +64,34 @@ ponder.on('Stablecoin:SetModule', async ({ event, context }) => {
 		module: event.args.newModule,
 
 		kind: 'Set',
-		message: 'Module changes approved',
+		message: 'Proposed changes applied',
 	});
+
+	const setMessageOverwrite = (module: Address, message: string) => {
+		module = module.toLowerCase() as Address;
+
+		if (module == '0x6d6525d8e234d840b2699f7107f14fa0d6c62c42') {
+			return 'Morpho Core Vault - V1';
+		} else if (module == '0x6f05782a28cda7f01b054b014cf6cd92023937e4') {
+			return 'Curve USDC Pool - V1';
+		} else if (module == '0xab6523cd7fa669ec35bd5358df505382b810cdb5') {
+			return 'Morpho Core Vault - V1.1';
+		} else if (module == '0x77ebb1d7a7f5371a61b7d21d7734b6dde6f0f94f') {
+			return 'Curve USDC Pool - V1.1';
+		} else if (module == '0xfb46481a9819e068af0eb64a2c2824fcecaaaa45') {
+			return 'Morpho Core Vault - V1.2';
+		} else if (module == '0x8f36bbee57acb4857cb97898020b529969fdf221') {
+			return 'TermMax Core Vault - RV1';
+		} else if (module == '0x77cbb2f180f55dd2916bfc78f879a2c2de37f638') {
+			return 'Curve USDC Pool - V1.1';
+		} else if (module == '0x397fb4a34757ac180c0841b26131f25040e2e50b') {
+			return 'TermMax RWA Vault - RV1';
+		} else if (module == '0x5febefd5aecfefb3352c5edd49d634b1456c4bd7') {
+			return 'TermMax Yield Vault - RV1';
+		} else {
+			return message;
+		}
+	};
 
 	if (proposals.length === 0) {
 		// native module approved during stablecoin deployment
@@ -77,7 +103,7 @@ ponder.on('Stablecoin:SetModule', async ({ event, context }) => {
 			blockheight: BigInt(event.block.number),
 			caller: event.args.caller,
 			module: event.args.newModule,
-			message: 'MorphoAdapterV1',
+			message: setMessageOverwrite(event.args.newModule, ''),
 
 			expiredAt: maxUint256,
 			updatedAt: BigInt(event.block.timestamp),
@@ -95,7 +121,10 @@ ponder.on('Stablecoin:SetModule', async ({ event, context }) => {
 				blockheight: BigInt(event.block.number),
 				caller: event.args.caller,
 				module: event.args.newModule,
-				message: proposal!.message,
+				message: setMessageOverwrite(
+					event.args.newModule,
+					proposal!.message,
+				),
 
 				expiredAt: proposal?.expiredAt || 0n,
 				updatedAt: BigInt(event.block.timestamp),
